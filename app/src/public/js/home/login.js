9e5665abd5ae7bae7799ee9f04d5251b3dff4531
miok-jung <miok.dev@gmail.com>
2021-12-28 22:00:46 +0900

    feat: DOM을 이용하여 HTML 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,19 @@
 "use strict";
 
-console.log("hellow");
+// 1. 유저가 아이디/비밀번호를 입력한다.
+// 2. 유저는 엔터 혹은 로그인 버튼을 클릭할 때, 로그인이 된다.
+// 질의 선택자?
+const id = document.querySelector("#id"),
+  password = document.querySelector("#password"),
+  loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+// 리퀘스트 변수?
+function login() {
+  const req = {
+    id: id.value,
+    password: password.value,
+  };
+  console.log(req);
+}
