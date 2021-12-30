41f9b90151bf5eeab606f6d9f2557f1236ebcc0f
miok-jung <miok.dev@gmail.com>
2021-12-30 21:17:25 +0900

    feat: 로그인을 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -15,5 +15,13 @@ function login() {
     id: id.value,
     password: password.value,
   };
-  console.log(req);
+  console.log("req1", req);
+  console.log("req2", JSON.stringify(req));
+  fetch("/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(req),
+  });
 }
