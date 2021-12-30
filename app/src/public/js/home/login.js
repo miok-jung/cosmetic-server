3f41b90ec8fd3adc707271343c7499b6e6a1c736
miok-jung <miok.dev@gmail.com>
2021-12-30 21:47:49 +0900

    feat: 프런트 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -28,6 +28,13 @@ function login() {
     // 따라서 한번더 then으로 접근을 해야한다.
     .then((res) => res.json())
     .then((res) => {
-      console.log("res", res);
+      if (res.success) {
+        location.href = "/";
+      } else {
+        alert(res.msg);
+      }
+    })
+    .catch((err) => {
+      console.error(new Error("로그인 중 에러 발생"));
     });
 }
