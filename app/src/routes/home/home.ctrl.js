420c161a945ac4e160e2a571e2606f2f0a8729ea
miok-jung <miok.dev@gmail.com>
2022-01-02 23:29:16 +0900

    feat: User 모델 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,7 +1,7 @@
 // ANCHOR home의 컨트롤러
 "use strict";
 
-const UserStorage = require("../../models/UserStorage");
+const User = require("../../models/User");
 
 const output = {
   home: (req, res) => {
@@ -14,21 +14,8 @@ const output = {
 
 const process = {
   login: (req, res) => {
-    const id = req.body.id,
-      password = req.body.password;
-    // const userStorage = new UserStorage();
-    const users = UserStorage.getUsers("id", "password");
-
-    const response = {};
-    if (users.id.includes(id)) {
-      const idx = users.id.indexOf(id);
-      if (users.password[idx] === password) {
-        response.success = true;
-        return res.json(response);
-      }
-    }
-    response.success = false;
-    response.msg = "로그인에 실패하셨습니다.";
+    const user = new User(req.body);
+    const response = user.login();
     return res.json(response);
   },
 };
