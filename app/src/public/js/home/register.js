a4beb2f26f039fbaeaf5543247be20e61cbe1f96
miok-jung <miok.dev@gmail.com>
2022-01-04 22:11:57 +0900

    feat: 회원가입 라우팅 구현

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -10,11 +10,13 @@ console.log("hello register");
 registerBtn.addEventListener("click", register);
 
 function register() {
+  if (!id.value) return alert("아이디를 입력해주십시오.");
+  if (password.value !== confirmPassword.value)
+    return alert("비밀번호가 일치하지 않습니다.");
   const req = {
     id: id.value,
     name: name.value,
     password: password.value,
-    confirmPassword: confirmPassword.value,
   };
   fetch("/register", {
     method: "POST",
