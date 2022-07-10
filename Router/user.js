5cc7742f28971e84c18c2ed12de2d234350afdce
miok-jung <miok.dev@gmail.com>
2022-07-10 20:48:51 +0900

    modify: logout - 특정 페이지 로그아웃 안되는 원인 체크 및

--- Router/user.js
+++ Router/user.js
@@ -1,34 +1,34 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
 // ANCHOR Models를 불러오기
-const { User } = require('../Models/User');
-const { auth } = require('../middleware/auth');
-const { Counter } = require('../Models/Counter');
+const { User } = require('../Models/User')
+const { auth } = require('../middleware/auth')
+const { Counter } = require('../Models/Counter')
 
 router.post('/register', (req, res) => {
-  let temp = req.body;
+  let temp = req.body
   Counter.findOne({ name: 'counter' })
     .then((doc) => {
       // doc => MongoDB counter이름의 정보를 가져온다..
-      temp.userNum = doc.userNum;
+      temp.userNum = doc.userNum
       // 인스턴스 생성 : 할당된 실체
-      const user = new User(req.body); // body-parser을 통해 json 형태를 가져올 수 있다.
+      const user = new User(req.body) // body-parser을 통해 json 형태를 가져올 수 있다.
       user.save().then(() => {
         Counter.updateOne({ name: 'counter' }, { $inc: { userNum: 1 } }).then(
           () => {
-            res.status(200).json({ success: true });
+            res.status(200).json({ success: true })
           },
-        );
-      });
+        )
+      })
     })
     .catch((err) => {
       return res.status(400).json({
         success: false,
         err,
-      });
-    });
-});
+      })
+    })
+})
 // FIXME 로그인 오류시 보내는 코드를 나누어야 할지 고민해야한다.
 // FIXME 400에러코드 전송시 번호는 무엇으로 해야될까?
 // FIXME 에러문구 재수정 필요
@@ -39,7 +39,7 @@ router.post('/login', (req, res) => {
       return res.status(400).json({
         loginSuccess: false,
         message: '제공된 이메일에 해당하는 유저가 없습니다.',
-      });
+      })
     }
 
     // 2. DB에서 요청한 이메일이 있는 경우, 비밀번호가 맞는 비밀번호인지 확인하기
@@ -48,22 +48,22 @@ router.post('/login', (req, res) => {
         return res.status(400).json({
           loginSuccess: false,
           message: '비밀번호가 틀렸습니다.',
-        });
+        })
 
       // 3. 비밀번호까지 같다면 토큰을 생성하기
       user.generateToken((err, user) => {
-        if (err) return res.status(400).send(err);
+        if (err) return res.status(400).send(err)
 
         // 토큰을 쿠키 저장한다. 쿠키, 로컬스토리지, 세션등 다양학 존재하며 각각의 장단점이 있다.
         res.cookie('x_auth', user.token).status(200).json({
           loginSuccess: true,
           userId: user._id,
           nickname: user.nickname,
-        });
-      });
-    });
-  });
-});
+        })
+      })
+    })
+  })
+})
 
 // NOTE auth : 미들웨어(중간작업)
 router.get('/auth', auth, (req, res) => {
@@ -76,13 +76,17 @@ router.get('/auth', auth, (req, res) => {
     email: req.user.email,
     nickname: req.user.nickname,
     role: req.user.role,
-  });
-});
+  })
+})
 
 router.get('/logout', auth, (req, res) => {
-  User.findOneAndUpdate({ _id: req.user._id }, { token: '' }, (err, user) => {
-    if (err) return res.json({ success: false, err });
-    return res.status(200).send({ success: true });
-  });
-});
-module.exports = router;
+  User.findOneAndUpdate({ token: '' })
+    .then((logout) => {
+      return res.status(200).send({ success: true, logout: logout })
+    })
+    .catch((err) => {
+      return res.json({ success: false, err })
+    })
+})
+
+module.exports = router
